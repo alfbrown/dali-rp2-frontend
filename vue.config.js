0a92d7105c31eed4c927b4131e57fa2445e262cd
Al <camrik55@yahoo.com>
2025-01-29 11:22:56 -0500

    update localhost

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port:8000,
     proxy: {
       '/api': {
         target: process.env.VUE_APP_API_BASE_URL,
